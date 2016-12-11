896fe11f4839e64923e05e19429a938b3bbc54c9
Matthew Preucil <mattpsbs@gmail.com>
2016-12-11 15:11:38 -0800

    Use only batch api. Add @escaping and throws.

--- Sources/pubsubcache.swift
+++ Sources/pubsubcache.swift
@@ -3,9 +3,36 @@ import Scope
 
 public protocol PubSubCache
 {
-	func set(value: Data, forKey: String) -> Void
-	func set(keysAndValues: [String : Data]) -> Void
-	func get(key: String) -> Data?
-	func subscribe(keys: Array<String>, onChange: ([String : Data]) -> ()) -> Scope
-	func subscribe(pattern: String, onChange: ([String : Data]) -> ()) -> Scope
+	func set(_ keysAndValues: [String : Data?]) throws -> Void
+	func get(_ keys: Array<String>) throws -> [String : Data?]
+	func subscribe(keys: Array<KeyMatch>, onChange: @escaping ([String : Data?]) -> ()) throws -> Scope
+}
+
+public enum KeyMatch
+{
+	case Exact(String)
+	case StartsWith(String)
+	case EndsWith(String)
+	case Contains(String)
+	case RegEx(String)
+}
+
+public extension KeyMatch
+{
+	public func matches(_ key: String) -> Bool {
+		switch self {
+		case .Exact(let pattern):
+			return pattern == key
+		case .StartsWith(let pattern):
+			return key.hasPrefix(pattern)
+		case .EndsWith(let pattern):
+			return key.hasSuffix(pattern)
+		case .Contains(let pattern):
+			return key.contains(pattern)
+		case .RegEx(let pattern):
+			let regex = try! NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
+			let matchRange = regex.rangeOfFirstMatch(in: key, options: [], range: NSMakeRange(0, key.characters.count))
+			return matchRange.location != NSNotFound
+		}
+	}
 }
